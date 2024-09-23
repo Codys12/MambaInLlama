ef295407fbd446d669f0795fcb4415bb76ec8947
Codys12 <41397239+Codys12@users.noreply.github.com>
2024-09-23 10:43:12 -0500

    update test

--- test.py
+++ test.py
@@ -4,83 +4,68 @@ from mamba2.hybrid_wrapper import MambaTransformerHybridModelWrapper
 from mamba2.hybrid_mamba_config import MambaConfig
 
 # Load the transformer model
-model_name = "meta-llama/Meta-Llama-3.1-8B"
-transformer_model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
+model_name = "./llama3_0.25_mamba2"
+#transformer_model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 # Load the hybrid model
-config = transformer_model.config
+#config = transformer_model.config
 
-if not hasattr(config, 'head_dim'):
-    d_xb = config.num_key_value_heads * \
-        (config.hidden_size // config.num_attention_heads)
-    d_inner = config.hidden_size
-    d_state = config.hidden_size//config.num_attention_heads
-else:
-    # to handle gemma2
-    d_xb = config.num_key_value_heads * config.head_dim
-    d_inner = config.num_attention_heads * config.head_dim
-    d_state = config.head_dim
+# if not hasattr(config, 'head_dim'):
+#     d_xb = config.num_key_value_heads * \
+#         (config.hidden_size // config.num_attention_heads)
+#     d_inner = config.hidden_size
+#     d_state = config.hidden_size//config.num_attention_heads
+# else:
+#     # to handle gemma2
+#     d_xb = config.num_key_value_heads * config.head_dim
+#     d_inner = config.num_attention_heads * config.head_dim
+#     d_state = config.head_dim
 
-ssm_layers = [i for i in range(32)]
-attn_layers = [i for i in range(config.num_hidden_layers) if i not in ssm_layers]
+# ssm_layers = [0, 4, 8, 12, 16, 20, 24, 28]#[i for i in range(32)]
+# attn_layers = [i for i in range(config.num_hidden_layers) if i not in ssm_layers]
 
-mamba_config = MambaConfig(
-    config.hidden_size,
-    {"expand": 1, "ngroups":config.num_attention_heads, "d_state": d_state},
-    config.rms_norm_eps,
-    d_inner=d_inner,
-    d_xb=d_xb,
-    intermediate_size=config.intermediate_size,
-    hidden_act=config.hidden_act,
-    n_layer=config.num_hidden_layers,
-    attn_layers=attn_layers,
-)
-hybrid_model = MambaTransformerHybridModelWrapper.init_distillation(None, model_name, mamba_config, attn_layers, torch.bfloat16)
+# mamba_config = MambaConfig(
+#     config.hidden_size,
+#     {"expand": 1, "ngroups":config.num_attention_heads, "d_state": d_state},
+#     config.rms_norm_eps,
+#     d_inner=d_inner,
+#     d_xb=d_xb,
+#     intermediate_size=config.intermediate_size,
+#     hidden_act=config.hidden_act,
+#     n_layer=config.num_hidden_layers,
+#     attn_layers=attn_layers,
+# )
+#hybrid_model = MambaTransformerHybridModelWrapper.init_distillation(None, model_name, mamba_config, attn_layers, torch.bfloat16)
 
-transformer_model.to("cuda")
+hybrid_model = MambaTransformerHybridModelWrapper.from_pretrained(model_name)
+
+#transformer_model.to("cuda")
 hybrid_model.to("cuda")
 
-# Prepare dummy input
-dummy_input = "Hello, world!"
-input_ids = tokenizer(dummy_input, return_tensors="pt").input_ids
-input_ids = input_ids.to("cuda")
+messages = [
+    {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
+    {"role": "user", "content": "Who are you?"},
+]
 
-# Generate with transformer model to get KV cache
-with torch.no_grad():
-    outputs = transformer_model.generate(
-        input_ids,
-        max_new_tokens=1,
-        use_cache=True,
-        return_dict_in_generate=True,
-        output_attentions=True
-    )
+input_ids = tokenizer.apply_chat_template(
+    messages,
+    add_generation_prompt=True,
+    return_tensors="pt"
+).to(hybrid_model.device)
 
-# Print KV cache shape
-kv_cache = outputs.past_key_values
-#print(f"KV cache shape: {[(k.shape, v.shape) for k, v in kv_cache[0]]}")
-#print(kv_cache[0][0].shape)
-#print(kv_cache[0][0])
+terminators = [
+    tokenizer.eos_token_id,
+    tokenizer.convert_tokens_to_ids("<|eot_id|>")
+]
 
-#print(kv_cache[0][1].shape)
-#print(kv_cache[0][1])
-
-with torch.no_grad():
-    # try:
-    ssm_outputs = hybrid_model.generate(
-        input_ids,
-        max_new_tokens=20,
-        #use_cache=True,
-        return_dict_in_generate=True,
-        output_attentions=True
-    )
-
-    print(tokenizer.decode(ssm_outputs.sequences[0]))
-    # except:
-    #     print("Error in SSM forward")
-
-#TODO: replace with attention and shortcut in forward. 
-#Worst case scenario, manually step through hidden states to get querys from transformer
-
-#print(outputs)
-#print(dir(outputs))
+outputs = hybrid_model.generate(
+    input_ids,
+    max_new_tokens=256,
+    eos_token_id=terminators,
+    do_sample=True,
+    temperature=0.6,
+    top_p=0.9,
+)
+response = outputs[0][input_ids.shape[-1]:]
+print(tokenizer.decode(response, skip_special_tokens=True))
