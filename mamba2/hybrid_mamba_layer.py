35f103dfb498aa58d533a841557757df40faeba0
Codys12 <41397239+Codys12@users.noreply.github.com>
2024-09-23 20:14:54 -0500

    rescale

--- mamba2/hybrid_mamba_layer.py
+++ mamba2/hybrid_mamba_layer.py
@@ -439,7 +439,7 @@ class Mamba2(nn.Module, PyTorchModelHubMixin):
         # self.count += 1
 
         #out = out + attn_output
-        #out = out / (2.0 * self.scale_factor)
+        out = out / (2.0 * self.scale_factor)
         #out = out.div(2.0 * self.scale_factor)
         return out
 
@@ -509,7 +509,7 @@ class Mamba2(nn.Module, PyTorchModelHubMixin):
         if d_mlp > 0:
             y = torch.cat([F.silu(z0) * x0, y], dim=-1)
         out = self.out_proj(y)
-        #out = out / (2.0 * self.scale_factor)
+        out = out / (2.0 * self.scale_factor)
         return out.unsqueeze(1), conv_state, ssm_state
 
     def allocate_inference_cache(self, batch_size, max_seqlen, dtype=None, **kwargs):
